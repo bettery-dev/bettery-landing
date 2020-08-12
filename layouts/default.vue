3253ef8e0487ba0bdaf07659dd60b5a0571e14d4
Sarmaged <sarmaged@gmail.com>
2020-08-12 19:14:49 +0300

    fix overflow descktop

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,17 +1,19 @@
 <template>
-  <div class="wrapper">
-    <nuxt />
+  <div class="overflow">
+    <div class="wrapper">
+      <nuxt />
 
-    <footer class="footer">
-      <a href="/"><img src="~/assets/img/logo_footer.svg" alt=""/></a>
+      <footer class="footer">
+        <a href="/"><img src="~/assets/img/logo_footer.svg" alt=""/></a>
 
-      <menu>
-        <a href="#">Правила</a>
-        <a href="#">Приватность</a>
-        <a href="#">Помощь</a>
-      </menu>
+        <menu>
+          <a href="#">Правила</a>
+          <a href="#">Приватность</a>
+          <a href="#">Помощь</a>
+        </menu>
 
-      <div class="copyright">Bettery.app © 2020</div>
-    </footer>
+        <div class="copyright">Bettery.app © 2020</div>
+      </footer>
+    </div>
   </div>
 </template>
