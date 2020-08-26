01b4c9346d9dbfca56eb8ec23d045520835fa13f
Sarmaged <sarmaged@gmail.com>
2020-08-27 00:36:24 +0300

    lazyLoad & optimize CSS size (third try!!!)

--- components/Subscribe/index.vue
+++ components/Subscribe/index.vue
@@ -74,4 +74,143 @@ export default {
 }
 </script>
 
-<style lang="scss" src="./_.scss" scoped />
+<style lang="scss" scoped>
+@import '~@/assets/css/var';
+
+.subscribe {
+  position: relative;
+  z-index: 1;
+
+  color: $dark;
+  text-align: center;
+
+  background: $yellow;
+  border-radius: 48px;
+  box-shadow: 0 0 1px 1px $yellow;
+
+  max-width: 864px;
+  margin: auto;
+
+  padding: 48px;
+
+  @media #{$laptop} {
+    max-width: 576px;
+  }
+  @media #{$tablet} {
+    max-width: 500px;
+    padding: 24px;
+    border-radius: 24px;
+  }
+}
+
+h2 {
+  @include h2;
+
+  padding: 0 48px;
+
+  margin-bottom: 32px;
+
+  @media #{$laptop} {
+    padding: 0;
+    margin-bottom: 16px;
+  }
+}
+p {
+  @include text;
+
+  margin-bottom: 32px;
+
+  @media #{$laptop} {
+    margin-bottom: 16px;
+  }
+}
+.email {
+  color: $dark;
+  @include input;
+  opacity: 0.5;
+}
+
+form {
+  position: relative;
+}
+
+input {
+  color: $white;
+  @include input;
+  font-weight: bold;
+
+  border: 0;
+  background: $dark;
+
+  width: 100%;
+  height: 96px;
+
+  padding: 0 48px;
+
+  border-radius: 48px;
+
+  outline: 0;
+
+  &::placeholder {
+    color: $white;
+    opacity: 0.25;
+    text-align: center;
+  }
+
+  @media #{$laptop} {
+    height: 48px;
+    margin-bottom: 16px;
+    text-align: center;
+  }
+}
+
+button {
+  cursor: pointer;
+
+  top: 0;
+  right: 12px;
+  bottom: 0;
+  position: absolute;
+
+  color: $dark;
+  @include input;
+  font-weight: bold;
+
+  background: $white;
+
+  height: 72px;
+
+  margin: auto;
+  padding: 0 24px;
+
+  outline: 0;
+  border: 0;
+  border-radius: 48px;
+
+  &:disabled {
+    cursor: default;
+
+    color: rgba($dark, 0.2);
+  }
+
+  @media #{$laptop} {
+    top: auto;
+    left: auto;
+    right: auto;
+    bottom: auto;
+    position: relative;
+
+    height: 48px;
+  }
+}
+
+.isNotValid {
+  @include text;
+
+  margin-top: 10px;
+}
+
+form.error input {
+  color: $error;
+}
+</style>
