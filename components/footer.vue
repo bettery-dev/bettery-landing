94491d9863e7e98855728f3d76c33e6a3e8f41d4
Pavel Makarenko <cryfall@gmail.com>
2020-12-02 20:23:58 +0300

    added yandex metrika

--- components/footer.vue
+++ components/footer.vue
@@ -3,6 +3,22 @@
     <footer class="footer">
       <a href="/"><img src="~/assets/img/logo_footer.svg" alt=""/></a>
 
+      <!-- Yandex.Metrika counter -->
+      <script type="text/javascript" >
+        (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+        ym(67867402, "init", {
+              clickmap:true,
+              trackLinks:true,
+              accurateTrackBounce:true,
+              webvisor:true
+        });
+      </script>
+      <noscript><div><img src="https://mc.yandex.ru/watch/67867402" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+      <!-- /Yandex.Metrika counter -->
+
       <!--<menu>
         <nuxt-link to="/privacy">Правила</nuxt-link>
         <nuxt-link to="/rules">Приватность</nuxt-link>
